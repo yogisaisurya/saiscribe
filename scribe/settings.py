8cc59309f8d90e8fbc229312fe7b1b02cadacd24
yogisaisurya <91649668+yogisaisurya@users.noreply.github.com>
2022-03-16 20:30:11 +0530

    Update settings.py

--- scribe/settings.py
+++ scribe/settings.py
@@ -79,7 +79,9 @@ WSGI_APPLICATION = 'scribe.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'saiscribe',
+        'USER': 'yogisaisurya',
+        'PASSWORD': 'sai@66999',
     }
 }
 
